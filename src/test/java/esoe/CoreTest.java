932e7621ba6e5356daac3f9c1f611e04974a8974
esoe <igor_nikulin@mail.ru>
2018-11-02 17:51:17 +0300

    заготовка для shards

---
+++ src/test/java/esoe/CoreTest.java
@@ -0,0 +1,20 @@
+package esoe;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class CoreTest
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
